3d069a17d46eeffbaff6ba0f2342fa3459caa0c3
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-05-13 00:05:37 +0100

    Refactorização do gerador - Nomes das vias em ficheiro à parte

--- src/viajar/gerador_informacao.py
+++ src/viajar/gerador_informacao.py
@@ -3,57 +3,20 @@ import os
 import requests
 import overpy
 
-# País
+import vias
 
-ANDORRA = "AD"
-ESPANHA = "ES"
-GIBRALTAR = "GI"
-PORTUGAL = "PT"
-
-# Sistema
+# Inserir aqui auto-estrada para ser analisada pelo gerador
+AUTO_ESTRADA_PARA_ANALISAR = vias.ES_EXA1
 
 PATH_BASE = os.path.dirname(os.path.realpath(__file__))
+NOME_AUTO_ESTRADA = AUTO_ESTRADA_PARA_ANALISAR[0]
 CHAVE_API_PATH = os.path.join(PATH_BASE, '../api_key.txt')
 PASTA_FICHEIROS_TEMP = os.path.join(PATH_BASE, 'tmp')
-LOCAL_PATH = PASTA_FICHEIROS_TEMP + '/local.csv'
-LOCAL_ESPANHA_PATH = PASTA_FICHEIROS_TEMP + '/local_espanha.csv'
-LOCAL_PORTUGAL_PATH = PASTA_FICHEIROS_TEMP + '/local_portugal.csv'
-MUNICIPIO_PATH = PASTA_FICHEIROS_TEMP + '/municipio.csv'
-COMARCA_PATH = PASTA_FICHEIROS_TEMP + '/comarca.csv'
-
-# Níveis administractivos do OpenStreetMap
-
-# Andorra
-PAROQUIA = 7
-
-# Espanha
-COMUNIDADE_AUTONOMA = 4
-PROVINCIA = 6
-COMARCA = 7
-MUNICIPIO = 8
-DISTRITO_ES = 9
-
-# Gibraltar
-GIBRALTAR_NIVEL_ADMIN = 4
-
-# Portugal
-ENTIDADE_INTERMUNICIPAL = 5  # Nos Açores e na Madeira identificam ilhas
-DISTRITO_PT = 6
-CONCELHO = 7
-FREGUESIA = 8
-ANTIGA_FREGUESIA = 9  # Existe dentro de uniões de antigas freguesias
-
-# Auto-estradas
-
-ES_A49 = ("A-49", "Autopista del Quinto Centenario", ESPANHA)
-ES_A5 = ("A-5", "Autovía del Suroeste", ESPANHA)
-ES_M40 = ("M-40", "Autopista de Circunvalación M-40", ESPANHA)
-PT_A2 = ("A2", "Autoestrada do Sul", PORTUGAL)
-PT_A5 = ("A5", "Autoestrada da Costa do Estoril", PORTUGAL)
-
-# Linhas ferroviárias
-
-LINHA_DO_NORTE = ("Linha do Norte", PORTUGAL)
+LOCAL_PATH = PASTA_FICHEIROS_TEMP + f'/{NOME_AUTO_ESTRADA}_local.csv'
+LOCAL_ESPANHA_PATH = PASTA_FICHEIROS_TEMP + f'/{NOME_AUTO_ESTRADA}_local_espanha.csv'
+LOCAL_PORTUGAL_PATH = PASTA_FICHEIROS_TEMP + f'/{NOME_AUTO_ESTRADA}_local_portugal.csv'
+MUNICIPIO_PATH = PASTA_FICHEIROS_TEMP + f'/{NOME_AUTO_ESTRADA}_municipio.csv'
+COMARCA_PATH = PASTA_FICHEIROS_TEMP + f'/{NOME_AUTO_ESTRADA}_comarca.csv'
 
 """
 Gera ficheiros .csv com informação de saídas de auto-estradas
@@ -67,7 +30,7 @@ class GeradorInformacao:
         with open(CHAVE_API_PATH, 'r') as f:
             self.api_key = f.readlines()[0]
 
-    def create_locais_ficheiros(self, auto_estrada_tuplo):
+    def create_ficheiros(self, auto_estrada_tuplo):
         auto_estrada_numero = self.get_auto_estrada_numero(auto_estrada_tuplo)
         saidas = self.get_auto_estradas_saidas(auto_estrada_tuplo)
         saidas_ordenadas = list(saidas.keys())
@@ -83,30 +46,30 @@ class GeradorInformacao:
                 altitude = self.get_altitude(latitude, longitude)
                 f.write(f'{auto_estrada_numero} - Saída {saida},{latitude},{longitude},{altitude},\n')
 
-        if self.get_auto_estrada_pais(auto_estrada_tuplo) == ESPANHA:
+        if self.get_auto_estrada_pais(auto_estrada_tuplo) == vias.ESPANHA:
             municipios = set()
             with open(LOCAL_ESPANHA_PATH, 'w') as f:
                 for saida in saidas_ordenadas:
                     latitude = saidas[saida][0]
                     longitude = saidas[saida][1]
-                    municipio = self.get_divisao_administractiva(latitude, longitude, MUNICIPIO)
-                    provincia = self.get_divisao_administractiva(latitude, longitude, PROVINCIA)
+                    municipio = self.get_divisao_administractiva(latitude, longitude, vias.MUNICIPIO)
+                    provincia = self.get_divisao_administractiva(latitude, longitude, vias.PROVINCIA)
                     f.write(f'{auto_estrada_numero} - Saída {saida},{municipio},{provincia}\n')
                     municipios.add(f'{municipio},{provincia},\n')
                     print(f'Saída {saida} terminada')
-            with open(MUNICIPIO_PATH, 'w') as f:
-                for municipio in municipios:
-                    f.write(municipio)
+            # with open(MUNICIPIO_PATH, 'w') as f:
+            #     for municipio in municipios:
+            #         f.write(municipio)
 
-        elif self.get_auto_estrada_pais(auto_estrada_tuplo) == PORTUGAL:
+        elif self.get_auto_estrada_pais(auto_estrada_tuplo) == vias.PORTUGAL:
             with open(LOCAL_PORTUGAL_PATH, 'w') as f:
                 for saida in saidas_ordenadas:
                     latitude = saidas[saida][0]
                     longitude = saidas[saida][1]
-                    freguesia = self.get_divisao_administractiva(latitude, longitude, ANTIGA_FREGUESIA)
+                    freguesia = self.get_divisao_administractiva(latitude, longitude, vias.ANTIGA_FREGUESIA)
                     if not freguesia:  # Freguesia já existia antes de 2013
-                        freguesia = self.get_divisao_administractiva(latitude, longitude, FREGUESIA)
-                    concelho = self.get_divisao_administractiva(latitude, longitude, CONCELHO)
+                        freguesia = self.get_divisao_administractiva(latitude, longitude, vias.FREGUESIA)
+                    concelho = self.get_divisao_administractiva(latitude, longitude, vias.CONCELHO)
                     f.write(f'{auto_estrada_numero} - Saída {saida},{freguesia},{concelho}\n')
                     print(f'Saída {saida} terminada')
 
@@ -126,6 +89,9 @@ class GeradorInformacao:
 
         saidas_temp = {}
         resultado = self.api.query(f'relation["name"="{auto_estrada_nome}"];(._;>;);out;')
+        if len(resultado.nodes) == 0:
+            # Auto-estradas mais pequenas não têm associada uma relação no OSM, são um conjunto de vias com o mesmo nome
+            resultado = self.api.query(f'way["name"="{auto_estrada_nome}"];(._;>;);out;')
         for node in resultado.nodes:
             if len(node.tags) > 0:
                 if node.tags.__contains__('highway') and node.tags['highway'] == 'motorway_junction':
@@ -156,4 +122,4 @@ class GeradorInformacao:
         return tuplo[2]  # Ex: ES
 
 
-GeradorInformacao().create_locais_ficheiros(ES_A5)
+GeradorInformacao().create_ficheiros(AUTO_ESTRADA_PARA_ANALISAR)
---
+++ src/viajar/vias.py
@@ -0,0 +1,50 @@
+"""
+Nomes de vias para serem processados pelo gerador de informação
+"""
+
+# País
+
+ANDORRA = "AD"
+ESPANHA = "ES"
+GIBRALTAR = "GI"
+PORTUGAL = "PT"
+
+# Níveis administractivos do OpenStreetMap
+
+# Andorra
+PAROQUIA = 7
+
+# Espanha
+COMUNIDADE_AUTONOMA = 4
+PROVINCIA = 6
+COMARCA = 7
+MUNICIPIO = 8
+DISTRITO_ES = 9
+
+# Gibraltar
+GIBRALTAR_NIVEL_ADMIN = 4
+
+# Portugal
+ENTIDADE_INTERMUNICIPAL = 5  # Nos Açores e na Madeira identificam ilhas
+DISTRITO_PT = 6
+CONCELHO = 7
+FREGUESIA = 8
+ANTIGA_FREGUESIA = 9  # Existe dentro de uniões de antigas freguesias
+
+# Auto-estradas
+
+ES_A483 = ("A-483", "Bollullos-Matalascañas", ESPANHA)
+ES_A49 = ("A-49", "Autopista del Quinto Centenario", ESPANHA)
+ES_A5 = ("A-5", "Autovía del Suroeste", ESPANHA)
+ES_A58 = ("A-58", "Autovía Trujillo-Cáceres", ESPANHA)
+ES_A66 = ("A-66", "Autovía Ruta de la Plata", ESPANHA)
+ES_EXA1 = ("EX-A1", "Autovía del Norte de Extremadura", ESPANHA)
+ES_EXA2 = ("EX-A2", "Autovía de las Vegas Altas", ESPANHA)
+ES_M40 = ("M-40", "Autopista de Circunvalación M-40", ESPANHA)
+
+PT_A2 = ("A2", "Autoestrada do Sul", PORTUGAL)
+PT_A5 = ("A5", "Autoestrada da Costa do Estoril", PORTUGAL)
+
+# Linhas ferroviárias
+
+LINHA_DO_NORTE = ("Linha do Norte", PORTUGAL)
