9def2cb188110981337f14a6d8655a41376ea7f5
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-10-28 01:06:23 +0100

    Função de Haversine adicionada e testada

---
+++ src/test/viajar/test_gerador_informacao.py
@@ -0,0 +1,110 @@
+import unittest
+
+from viajar import haversine
+
+NORTE = haversine.NORTE
+NORDESTE = haversine.NORDESTE
+ESTE = haversine.ESTE
+SUDESTE = haversine.SUDESTE
+SUL = haversine.SUL
+SUDOESTE = haversine.SUDOESTE
+OESTE = haversine.OESTE
+NOROESTE = haversine.NOROESTE
+
+obter_ponto_cardeal = haversine.obter_ponto_cardeal
+
+
+class TestGeradorInformacao(unittest.TestCase):
+
+    def test_obter_ponto_cardeal_mesmo_ponto(self):
+        self.assertEqual("", obter_ponto_cardeal((39.0, 0), (39.0, 0)))
+
+    def test_obter_ponto_cardeal_mesma_longitude(self):
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (39.5, 0)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38.5, 0)))
+
+    def test_obter_ponto_cardeal_mesma_latitude(self):
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39.0, 1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39.0, -1)))
+
+    def test_obter_ponto_cardeal_diagonais(self):
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40.0, 0)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.1)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.2)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.3)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.4)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.5)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.6)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.7)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.8)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 0.9)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (40.0, 1)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (39.9, 1)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (39.8, 1)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (39.7, 1)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (39.6, 1)))
+        self.assertEqual(NORDESTE, obter_ponto_cardeal((39.0, 0), (39.5, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39.4, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39.3, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39.2, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39.1, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (39, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (38.9, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (38.8, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (38.7, 1)))
+        self.assertEqual(ESTE, obter_ponto_cardeal((39.0, 0), (38.6, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38.5, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38.4, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38.3, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38.2, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38.1, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 1)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 0.9)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 0.8)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 0.7)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 0.6)))
+        self.assertEqual(SUDESTE, obter_ponto_cardeal((39.0, 0), (38, 0.5)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, 0.4)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, 0.3)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, 0.2)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, 0.1)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, 0)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, -0.1)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, -0.2)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, -0.3)))
+        self.assertEqual(SUL, obter_ponto_cardeal((39.0, 0), (38, -0.4)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -0.5)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -0.6)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -0.7)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -0.8)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -0.9)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38, -1)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38.1, -1)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38.2, -1)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38.3, -1)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38.4, -1)))
+        self.assertEqual(SUDOESTE, obter_ponto_cardeal((39.0, 0), (38.5, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (38.6, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (38.7, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (38.8, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (38.9, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39.1, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39.2, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39.3, -1)))
+        self.assertEqual(OESTE, obter_ponto_cardeal((39.0, 0), (39.4, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (39.5, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (39.6, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (39.7, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (39.8, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (39.9, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -1)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -0.9)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -0.8)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -0.7)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -0.6)))
+        self.assertEqual(NOROESTE, obter_ponto_cardeal((39.0, 0), (40, -0.5)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40, -0.4)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40, -0.3)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40, -0.2)))
+        self.assertEqual(NORTE, obter_ponto_cardeal((39.0, 0), (40, -0.1)))
---
+++ src/viajar/haversine.py
@@ -0,0 +1,101 @@
+from math import sin, cos, sqrt, asin, pi
+
+#  Pontos cardeais
+NORTE = "N"
+NORDESTE = "NE"
+ESTE = "E"
+SUDESTE = "SE"
+SUL = "S"
+SUDOESTE = "SO"
+OESTE = "O"
+NOROESTE = "NO"
+
+RAIO_TERRA = 6371  # km
+
+
+def obter_ponto_cardeal(origem: (float, float), destino: (float, float)) -> str:
+    """
+    Dada uma origem e um destino, retorna o ponto cardeal do destino em relação à origem
+    Ex: Se origem = (39.0, 0.0) e destino = (40.0, 0.0), retorno será "N"
+    :param origem: Coordenadas decimais
+    :param destino: Idem
+    :return: Abreviatura do ponto cardeal, ou "" se origem e destino forem iguais
+    """
+    def _obter_distancia_haversine(origem: (float, float), destino: (float, float)) -> float:
+        """
+        Retorna distância em linha recta em km entre origem e destino. Erro até 0.5%
+        """
+        def _para_radianos(decimal: float) -> float:
+            return decimal * pi / 180
+
+        latitude_origem, longitude_destino = origem
+        latitude_destino, longitude_destino = destino
+        diff_lat: float = destino[0] - origem[0]
+        diff_lon: float = destino[1] - origem[1]
+
+        parametro_1: float = sin(_para_radianos(diff_lat / 2)) ** 2
+        parametro_2: float = cos(_para_radianos(latitude_origem)) * cos(_para_radianos(latitude_destino)) * (
+                sin(_para_radianos(diff_lon / 2)) ** 2)
+        distancia: float = 2 * RAIO_TERRA * asin(sqrt(parametro_1 + parametro_2))
+
+        return distancia
+
+    if origem == destino:  # Mesmo ponto
+        return ""
+
+    distancia_norte_sul: float = _obter_distancia_haversine((origem[0], 0), (destino[0], 0))  # Será sempre >= 0
+    distancia_este_oeste: float = _obter_distancia_haversine((0, origem[1]), (0, destino[1]))  # Será sempre >= 0
+
+    diff_lat: float = destino[0] - origem[0]
+    diff_lon: float = destino[1] - origem[1]
+
+    if diff_lat == 0:
+        if diff_lon > 0:
+            return ESTE
+        elif diff_lon < 0:
+            return OESTE
+    elif diff_lon == 0:
+        if diff_lat > 0:
+            return NORTE
+        elif diff_lat < 0:
+            return SUL
+    elif diff_lat == diff_lon:
+        if diff_lat > 0 and diff_lon > 0:
+            return NORDESTE
+        elif diff_lat < 0 and diff_lon > 0:
+            return SUDESTE
+        elif diff_lat < 0 and diff_lon < 0:
+            return SUDOESTE
+        elif diff_lat > 0 and diff_lon < 0:
+            return NOROESTE
+    else:
+        if diff_lat > 0 and diff_lon > 0:  # N, NE, E
+            if abs(distancia_norte_sul) > 2 * abs(distancia_este_oeste):
+                return NORTE
+            elif abs(distancia_norte_sul) < 0.5 * abs(distancia_este_oeste):
+                return ESTE
+            else:
+                return NORDESTE
+        elif diff_lat < 0 and diff_lon > 0:  # E, SE, S
+            if abs(distancia_norte_sul) > 2 * abs(distancia_este_oeste):
+                return SUL
+            elif abs(distancia_norte_sul) < 0.5 * abs(distancia_este_oeste):
+                return ESTE
+            else:
+                return SUDESTE
+        elif diff_lat < 0 and diff_lon < 0:  # S, SO, O
+            if abs(distancia_norte_sul) > 2 * abs(distancia_este_oeste):
+                return SUL
+            elif abs(distancia_norte_sul) < 0.5 * abs(distancia_este_oeste):
+                return OESTE
+            else:
+                return SUDOESTE
+        elif diff_lat > 0 and diff_lon < 0:  # O, NO, N
+            if abs(distancia_norte_sul) > 2 * abs(distancia_este_oeste):
+                return NORTE
+            elif abs(distancia_norte_sul) < 0.5 * abs(distancia_este_oeste):
+                return OESTE
+            else:
+                return NOROESTE
+
+    return ""
