ca8701b8be52a4664b6cb649807491a942666d5e
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-12-07 01:41:41 +0000

    Conclusão da Linha de Sintra e da Linha de Cintura

--- src/gerador_info_main.py
+++ src/gerador_info_main.py
@@ -1,4 +1,4 @@
 import viajar.gerador_informacao as gerador
 import viajar.vias as vias
 
-gerador.GeradorInformacao(vias.PT_LINHA_DE_SINTRA)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
+gerador.GeradorInformacao(vias.PT_LINHA_DO_OESTE)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
