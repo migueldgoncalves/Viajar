1ffbb0c7bc2829874e26c2c013050680285943b2
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2021-12-27 02:36:50 +0000

    Novas saídas da A-92 e A-92N em Granada

--- src/gerador_info_main.py
+++ src/gerador_info_main.py
@@ -1,4 +1,4 @@
 import viajar.gerador_informacao as gerador
 import viajar.vias as vias
 
-gerador.GeradorInformacao(vias.ES_A92)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
+gerador.GeradorInformacao(vias.ES_A92N)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
