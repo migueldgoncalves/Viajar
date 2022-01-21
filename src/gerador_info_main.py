2392df3a2b1a7ac82632b90752500a7af31d6d73
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-01-21 01:07:41 +0000

    Novas saídas da M-40 em Madrid

--- src/gerador_info_main.py
+++ src/gerador_info_main.py
@@ -1,4 +1,4 @@
 import viajar.gerador_informacao as gerador
 import viajar.vias as vias
 
-gerador.GeradorInformacao(vias.ES_A66)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
+gerador.GeradorInformacao(vias.ES_M40)  # Inserir aqui estrada ou linha ferroviária para ser analisada pelo gerador
