6ce57a63ebe3745177b7a8ed355cf6ff0878e3f5
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-02-11 01:36:58 +0000

    Novas saídas da H-30, H-31 e A-497 em Huelva

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ Main - Executar para aceder a todos os scripts disponíveis
 
 import viajar.vias as vias
 # INSERIR AQUI estrada ou linha ferroviária para ser analisada pelo gerador
-VIA = vias.ES_A7
+VIA = vias.ES_A497
 
 OPCAO_SAIR = 0
 
