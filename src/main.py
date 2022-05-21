79fee9865cf3107208d90c72b78d5b6083ad3ede
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-05-21 01:54:28 +0100

    Conclusão do Ramal de Tomar e da Linha do Leste

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ Main - Executar para aceder a todos os scripts disponíveis
 
 import viajar.auxiliar.vias as vias
 # INSERIR AQUI estrada ou linha ferroviária para ser analisada pelo gerador
-VIA = vias.PT_LINHA_DO_NORTE
+VIA = vias.PT_LINHA_DO_LESTE
 
 OPCAO_SAIR = 0
 
