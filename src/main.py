59d3221b5eb501cde2ed7a4048889e3781011ccd
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-09-06 00:48:24 +0100

    2526 locations - New exits in the AP-9 in Pontevedra

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def exit_program():
 INITIAL_LOCATION = 'Guerreiros do Rio'
 
 # INSERT HERE road or railway to be analysed by the automatic generator of information
-WAY_TO_PROCESS = ways.ES_R2
+WAY_TO_PROCESS = ways.ES_AP9
 
 OPTION_TRAVEL = 1
 OPTION_CAR = 2
