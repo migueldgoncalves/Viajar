175e31945df7534878cb8e031edad6f75afe8557
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-10-16 02:44:45 +0100

    2775 locations - Conclusion of the Guimarães Line

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def exit_program():
 INITIAL_LOCATION = 'Guerreiros do Rio'
 
 # INSERT HERE road or railway to be analysed by the automatic generator of information
-WAY_TO_PROCESS = ways.PT_LINHA_DO_MINHO
+WAY_TO_PROCESS = ways.PT_LINHA_DE_GUIMARAES
 
 OPTION_TRAVEL = 1
 OPTION_CAR = 2
