d80a6b5840d35ddd8e760d203f9fd8c87cabc140
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2022-10-23 02:16:40 +0100

    2809 locations - Conclusion of the Portuguese active passenger rail network

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def exit_program():
 INITIAL_LOCATION = 'Guerreiros do Rio'
 
 # INSERT HERE road or railway to be analysed by the automatic generator of information
-WAY_TO_PROCESS = ways.PT_RAMAL_DE_BRAGA
+WAY_TO_PROCESS = ways.PT_LINHA_DO_MINHO
 
 OPTION_TRAVEL = 1
 OPTION_CAR = 2
