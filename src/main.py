1bd3f454ea8252c97b4306ad48dd4dbcd781a324
Miguel Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2024-02-16 08:46:04 +0000

    3492 locations - Added flights to Porto Airport

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ Main - Execute to access all available scripts
 
 
 # INSERT HERE location where to start journey
-INITIAL_LOCATION = 'Guerreiros do Rio'
+INITIAL_LOCATION = 'Aeroporto do Porto'
 
 # INSERT HERE road or railway to be analysed by the automatic generator of information
 WAY_TO_PROCESS = ways.PT_PORTO_METRO_LINE_E
